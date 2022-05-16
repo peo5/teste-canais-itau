5d09845a5c8c1340e640155322d30303f6cccbb7
peo5 <pedromsimoesferreira@gmail.com>
2022-05-16 12:18:57 -0300

    Refactored read_entries

--- my_program/main.py
+++ my_program/main.py
@@ -162,7 +162,8 @@ def read_entries(input_file_name: str):
     """Lê os registros de um arquivo
         os campos de cada registro devem ser separados por |
         o nome de cada campo deve ser especificado na primeira linha do arquivo
-        os nomes devem estar ordenados e separados por |
+        a ordem dos campos deve corresponder à ordem dos dados 
+        assim como os campos, os nomes devem ser separados por |
         """
 
     with open(input_file_name, 'r') as input_file: 
@@ -175,10 +176,10 @@ def read_entries(input_file_name: str):
                 continue
             
             split_line = input_line.split('|')
-            named_data = zip(row_names, split_line)
-            data = {row_name:data.strip() for row_name, data in named_data}
+            named_entry = zip(row_names, split_line)
+            entry = {row_name:data.strip() for row_name, data in named_data}
 
-            yield data
+            yield entry
 
 
 def execute_transaction(transaction: Transaction):
